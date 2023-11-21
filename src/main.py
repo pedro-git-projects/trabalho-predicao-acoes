9cd8909c2e29309bc76ff1a2bfc809ed4ffe04e8
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-11-20 22:52:37 -0300

    implementando comparador #10

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+from comparador.comparador import Comparador
 
 from preditor.preditor import Preditor
 
@@ -6,18 +7,25 @@ from preditor.preditor import Preditor
 def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
-    
+
+    comparador = Comparador()
+
     # Regrassão linear
-    preditor_linear = Preditor(intervalo_datas=None, modelo='linear')
+    preditor_linear = Preditor(intervalo_datas=None, modelo="linear")
     preditor_linear.fit()
     preditor_linear.predict()
-    print(f'Raiz quadrada do erro médio: {preditor_linear.rmse()}')
+    comparador.adicionar_modelo("Linear Regression", preditor_linear)
+    print(f"Raiz quadrada do erro médio: {preditor_linear.rmse()}")
 
     # Regressão vetorial de suporte epsilon
-    preditor_svm = Preditor(intervalo_datas=None, modelo='svm')
+    preditor_svm = Preditor(intervalo_datas=None, modelo="svm")
     preditor_svm.fit()
     preditor_svm.predict()
-    print(f'SVM RMSE: {preditor_svm.rmse()}')
+    comparador.adicionar_modelo("SVM", preditor_svm)
+    print(f"SVM RMSE: {preditor_svm.rmse()}")
+
+    comparador.plotar_resultados()
+
 
 if __name__ == "__main__":
     main()
